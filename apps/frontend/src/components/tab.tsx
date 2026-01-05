30213a4d6a7aa6a977d857b982222a91541e7765
tsaaiiiii <a0909513991@gmail.com>
2026-01-05 22:57:36 +0800

    feat: 新增 tab 畫面與路由切換功能

---
+++ apps/frontend/src/components/tab.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from '@tanstack/react-router'
+import { clsx } from 'clsx'
+
+export function Tab() {
+  const router = useRouter()
+  const RouteButtons = [
+    {
+      name: '首頁',
+      href: '/',
+    },
+    {
+      name: '菜單組',
+      href: '/menuset',
+    },
+    {
+      name: '菜譜',
+      href: '/menu',
+    },
+    {
+      name: '我的',
+      href: '/user',
+    },
+  ]
+
+  const currentRoute = router.state.location.pathname
+
+  return (
+    <div className="absolute bottom-0 w-full">
+      <div className="flex justify-center px-4 py-2 bg-offWhite gap-1">
+        {RouteButtons.map((item) => (
+          <a
+            href={item.href}
+            key={item.name}
+            className={clsx(
+              'w-20 h-10 rounded-4xl flex items-center justify-center text-sm',
+              { 'bg-orange-100': currentRoute === item.href },
+              currentRoute === item.href
+                ? 'text-black font-medium'
+                : 'text-content',
+            )}
+          >
+            {item.name}
+          </a>
+        ))}
+      </div>
+    </div>
+  )
+}
--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -1,13 +1,18 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { Tab } from '@/components/tab.tsx'
 
 export const Route = createRootRoute({
   component: RootLayout,
+  notFoundComponent: () => <div className="p-6">404 Not Found</div>,
 })
 
 function RootLayout() {
   return (
-    <div className="min-h-dvh bg-earthTone-50 font-sans-tc text-primary">
-      <Outlet />
-    </div>
+    <>
+      <div className="relative min-h-dvh bg-earthTone-50 font-sans-tc text-primary">
+        <Outlet />
+      </div>
+      <Tab />
+    </>
   )
 }
